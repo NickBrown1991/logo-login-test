1531aefbae165b4b68d8caab6b08587f6f90593b
Nick Brown <nick_brown_1991@hotmail.com>
2022-06-15 20:01:56 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,50 +1,15 @@
-import React, { Component } from "react"
-import logo from "./logo.svg"
-import "./App.css"
+import logo from './Assets/arcane-alchemist-logo.png';
+import './App.css';
+import Tilt from 'react-vanilla-tilt';
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
-
-  handleClick = api => e => {
-    e.preventDefault()
-
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
-
-  render() {
-    const { loading, msg } = this.state
-
-    return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    )
-  }
+function App() {
+  return (
+    <div className="App">
+        <Tilt options={{ scale: 2, max: 25, glare: true, perspective: 1}}>
+         <img src={logo} className="App-logo" alt="logo" />
+        </Tilt>
+    </div>
+  );
 }
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
-        </header>
-      </div>
-    )
-  }
-}
-
-export default App
+export default App;
